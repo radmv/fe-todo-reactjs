81d32c6bcf9a8f3ea5e4491dc6c0817910cb4838
Angga rizki <radmdev@gmail.com>
2024-03-16 17:10:24 +0700

    7#conditional rendering

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,16 +1,9 @@
-import { Link } from "react-router-dom";
 import { AuthLayout, FormLogin } from "../components";
 
 const LoginPage = () => {
   return (
-    <AuthLayout title="Login">
+    <AuthLayout title="Login" type="login">
       <FormLogin />
-      <p className="text-sm mt-5 text-center">
-        Don{"'"}t have an account?{" "}
-        <Link to={"/register"} className="font-bold text-blue-600">
-          Register
-        </Link>
-      </p>
     </AuthLayout>
   );
 };
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,16 +1,9 @@
-import { Link } from "react-router-dom";
 import { AuthLayout, FormRegister } from "../components";
 
 const RegisterPage = () => {
   return (
-    <AuthLayout title="Register">
+    <AuthLayout title="Register" type="register">
       <FormRegister />
-      <p className="text-sm mt-5 text-center">
-        Have an account?{" "}
-        <Link to={"/login"} className="font-bold text-blue-600">
-          Login
-        </Link>
-      </p>
     </AuthLayout>
   );
 };
